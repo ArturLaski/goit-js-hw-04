d7b7a501dd4fa8364142e4b9a330eca7d044714b
ArturLaski <162509395+ArturLaski@users.noreply.github.com>
2024-10-21 20:40:28 +0200

    update result count

--- js/task-3.js
+++ js/task-3.js
@@ -19,10 +19,10 @@ const task3ResultsDiv = document.getElementById('task-3-results');
 
 // Test cases
 profile.changeUsername("Marco");
-let result1 = profile.getInfo(); // "Marco has 300 active hours!"
+let result7 = profile.getInfo(); // "Marco has 300 active hours!"
 
 profile.updatePlayTime(20);
-let result2 = profile.getInfo(); // "Marco has 320 active hours!"
+let result8 = profile.getInfo(); // "Marco has 320 active hours!"
 
-task3ResultsDiv.innerHTML += `<p>First Info: ${result1}</p>`;
-task3ResultsDiv.innerHTML += `<p>Second Info: ${result2}</p>`;
+task3ResultsDiv.innerHTML += `<p>First Info: ${result7}</p>`;
+task3ResultsDiv.innerHTML += `<p>Second Info: ${result8}</p>`;
