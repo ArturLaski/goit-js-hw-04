76b63afa03d8289f77482f3f5f59a6fc06a873b9
ArturLaski <162509395+ArturLaski@users.noreply.github.com>
2024-10-21 20:26:56 +0200

    update error Task2 & 3

--- js/task-2.js
+++ js/task-2.js
@@ -1,37 +1,36 @@
 function calcAverageCalories(days) {
-    if (days.length === 0) return 0;
-  
-    const totalCalories = days.reduce((acc, day) => acc + day.calories, 0);
-    return totalCalories / days.length;
-  }
-  
-  const task2ResultsDiv = document.getElementById('task-2-results');
-  
-  const days1 = [
-    { day: 'monday', calories: 3010 },
-    { day: 'tuesday', calories: 3200 },
-    { day: 'wednesday', calories: 3120 },
-    { day: 'thursday', calories: 2900 },
-    { day: 'friday', calories: 3450 },
-    { day: 'saturday', calories: 3280 },
-    { day: 'sunday', calories: 3300 }
-  ];
-  
-  const days2 = [
-    { day: 'monday', calories: 2040 },
-    { day: 'tuesday', calories: 2270 },
-    { day: 'wednesday', calories: 2420 },
-    { day: 'thursday', calories: 1900 },
-    { day: 'friday', calories: 2370 },
-    { day: 'saturday', calories: 2280 },
-    { day: 'sunday', calories: 2610 }
-  ];
-  
-  const result1 = calcAverageCalories(days1); // 3180
-  const result2 = calcAverageCalories(days2); // 2270
-  const result3 = calcAverageCalories([]);    // 0
-  
-  task2ResultsDiv.innerHTML += `<p>${result1}</p>`;
-  task2ResultsDiv.innerHTML += `<p>${result2}</p>`;
-  task2ResultsDiv.innerHTML += `<p>${result3}</p>`;
-  
+  if (days.length === 0) return 0;
+
+  const totalCalories = days.reduce((acc, day) => acc + day.calories, 0);
+  return totalCalories / days.length;
+}
+
+const task2ResultsDiv = document.getElementById('task-2-results');
+
+const days1 = [
+  { day: 'monday', calories: 3010 },
+  { day: 'tuesday', calories: 3200 },
+  { day: 'wednesday', calories: 3120 },
+  { day: 'thursday', calories: 2900 },
+  { day: 'friday', calories: 3450 },
+  { day: 'saturday', calories: 3280 },
+  { day: 'sunday', calories: 3300 }
+];
+
+const days2 = [
+  { day: 'monday', calories: 2040 },
+  { day: 'tuesday', calories: 2270 },
+  { day: 'wednesday', calories: 2420 },
+  { day: 'thursday', calories: 1900 },
+  { day: 'friday', calories: 2370 },
+  { day: 'saturday', calories: 2280 },
+  { day: 'sunday', calories: 2610 }
+];
+
+const result1 = calcAverageCalories(days1); // 3180
+const result2 = calcAverageCalories(days2); // 2270
+const result3 = calcAverageCalories([]);    // 0
+
+task2ResultsDiv.innerHTML += `<p>Average calories for days1: ${result1}</p>`;
+task2ResultsDiv.innerHTML += `<p>Average calories for days2: ${result2}</p>`;
+task2ResultsDiv.innerHTML += `<p>Average calories for empty list: ${result3}</p>`;
--- js/task-3.js
+++ js/task-3.js
@@ -1,29 +1,28 @@
 const profile = {
-    username: "Jacob",
-    playTime: 300,
-    
-    changeUsername(newName) {
-      this.username = newName;
-    },
+  username: "Jacob",
+  playTime: 300,
   
-    updatePlayTime(hours) {
-      this.playTime += hours;
-    },
-  
-    getInfo() {
-      return `${this.username} has ${this.playTime} active hours!`;
-    }
-  };
-  
-  const task3ResultsDiv = document.getElementById('task-3-results');
-  
-  // Test cases
-  profile.changeUsername("Marco");
-  let result1 = profile.getInfo(); // "Marco has 300 active hours!"
-  
-  profile.updatePlayTime(20);
-  let result2 = profile.getInfo(); // "Marco has 320 active hours!"
-  
-  task3ResultsDiv.innerHTML += `<p>${result1}</p>`;
-  task3ResultsDiv.innerHTML += `<p>${result2}</p>`;
-  
+  changeUsername(newName) {
+    this.username = newName;
+  },
+
+  updatePlayTime(hours) {
+    this.playTime += hours;
+  },
+
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  }
+};
+
+const task3ResultsDiv = document.getElementById('task-3-results');
+
+// Test cases
+profile.changeUsername("Marco");
+let result1 = profile.getInfo(); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+let result2 = profile.getInfo(); // "Marco has 320 active hours!"
+
+task3ResultsDiv.innerHTML += `<p>First Info: ${result1}</p>`;
+task3ResultsDiv.innerHTML += `<p>Second Info: ${result2}</p>`;
